cea779557eec159990f3aec0971784153478a3e0
Sandhya Rajwanshi <47480884+SandhyaR1007@users.noreply.github.com>
2025-06-11 22:53:27 +0530

    test: write basic tests for search input

---
+++ src/tests/SearchInput.test.tsx
@@ -0,0 +1,42 @@
+import { fireEvent, render, screen, waitFor } from '@testing-library/react';
+import { SearchInput } from '../components';
+import '@testing-library/jest-dom';
+
+global.fetch = jest.fn(); // override fetch for all tests
+
+const typedFetch = global.fetch as jest.Mock;
+describe('SearchInput', () => {
+  let inputElement: HTMLInputElement, searchBtn: HTMLButtonElement;
+  beforeEach(() => {
+    typedFetch.mockClear();
+    render(<SearchInput />);
+    inputElement = screen.getByLabelText(/search/i);
+    searchBtn = screen.getByRole('button', { name: /search/i });
+  });
+  it('renders input and search button', () => {
+    expect(inputElement).toBeInTheDocument();
+    expect(searchBtn).toBeInTheDocument();
+  });
+
+  it('shows profile for valid username', async () => {
+    typedFetch.mockResolvedValueOnce({
+      ok: true,
+      json: async () => ({
+        login: 'sandhya',
+        name: 'Sandhya Rajwanshi',
+        avatar_url: 'https://...',
+        location: 'India',
+        html_url: 'https://github.com/sandhya',
+      }),
+    });
+
+    fireEvent.change(inputElement, {
+      target: { value: 'sandhya' },
+    });
+    fireEvent.click(searchBtn);
+
+    await waitFor(() => {
+      expect(screen.getByText(/sandhya rajwanshi/i)).toBeInTheDocument();
+    });
+  });
+});
