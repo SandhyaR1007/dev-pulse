c3327995cb308295ca86748c98f6171a324f6a17
Sandhya Rajwanshi <47480884+SandhyaR1007@users.noreply.github.com>
2025-06-13 22:51:04 +0530

    update: add loading indicator

--- src/components/SearchInput.tsx
+++ src/components/SearchInput.tsx
@@ -6,9 +6,11 @@ import ProfileCard from './ProfileCard';
 const SearchInput: React.FC = () => {
   const [username, setUsername] = useState<string>('');
   const [userProfile, setUserProfile] = useState<GithubUser | null>(null);
+  const [loading, setLoading] = useState<boolean>(false);
 
   const handleSearch = async () => {
     if (!username.length) return;
+    setLoading(true);
     try {
       const response = await fetchUserDetails(username);
       if (response) {
@@ -16,18 +18,23 @@ const SearchInput: React.FC = () => {
       }
     } catch (error) {
       console.log(error);
+    } finally {
+      setLoading(false);
     }
   };
   return (
     <div>
       <div className="flex gap-5 p-3">
+        <label htmlFor="search">Search</label>
         <input
+          id="search"
           className="p-1 w-100 border-2 border-slate-300 rounded-xl outline-0"
           placeholder="Search a username"
           type="text"
           value={username}
           onChange={(e) => setUsername(e.target.value)}
         />
+
         <button
           className="bg-purple-600 text-white text-md px-5 py-1 rounded-sm"
           onClick={handleSearch}
@@ -35,7 +42,11 @@ const SearchInput: React.FC = () => {
           Search
         </button>
       </div>
-      <ProfileCard userProfile={userProfile} />
+      {loading ? (
+        <h4>Loading...</h4>
+      ) : (
+        <ProfileCard userProfile={userProfile} />
+      )}
     </div>
   );
 };
